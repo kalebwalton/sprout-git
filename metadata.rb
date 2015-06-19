06dc60ab566d3ae64229808a9b039d758c45766d
Kaleb Walton <kalebwalton@gmail.com>
2015-06-19 12:42:38 -0400

    Removed depends home-brew since Chef 12.3 is native

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,3 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 supports         'mac_os_x'
 depends          'sprout-base'
-depends          'homebrew'
